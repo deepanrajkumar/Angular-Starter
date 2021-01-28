c0745b17ed317c35b0917d051e24295ae771cb4a
Deepan Raj Kumar <princedeepan@gmail.com>
2021-01-29 00:15:07 +0530

    updating server files

--- config/server.js
+++ config/server.js
@@ -1,20 +1,10 @@
-const express = require('express');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-
+const express = require("express");
 const app = express();
-const config = require('../webpack.config.js');
-const compiler = webpack(config);
 
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
-app.use(
-  webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath,
-  })
-);
+const port = 3000;
 
-// Serve the files on port 3000.
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!\n');
+app.use(express.static("./dist"));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
